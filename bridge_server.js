e59e88832380f93c81370cf6b8cf4e7418824eb3
RichardSignBlast <richard@signblast.com.au>
2025-01-23 09:27:20 +1000

    Update bridge_server.js

--- bridge_server.js
+++ bridge_server.js
@@ -21,7 +21,7 @@ function parseDeviceMessage(message) {
 
 // Function to send registration response
 function sendRegistrationResponse(socket, deviceId) {
-  const response = Buffer.from(`a5${deviceId}00e832ffed0110013002ae`, 'hex');
+  const response = Buffer.from(`a54350423431313032323300e832ffed0110013002ae`, 'hex');
   socket.write(response, (err) => {
     if (err) {
       console.error('Error sending registration response:', err);
