4de27dff7bac6c76ca4bc28546a4d776db7f41c5
RichardSignBlast <richard@signblast.com.au>
2025-01-23 13:52:17 +1000

    Update bridge_server.js

--- bridge_server.js
+++ bridge_server.js
@@ -5,7 +5,7 @@ const CLIENT_PORT = 8080;
 const CLIENT_HOST = '0.0.0.0';
 
 function sendRegistrationResponse(socket, clientInfo) {
-  const response = Buffer.from('a54350423431313032323300e832ffed01100031fdae', 'hex');
+  const response = Buffer.from('a54350423431313032323300e832ffed0010009602ae', 'hex');
 
   socket.write(response, (err) => {
     if (err) {
